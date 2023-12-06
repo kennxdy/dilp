8a44210acd061ed1efa60b4faee23467977e0ffa
Kennedy <kennedyinu@gmail.com>
2023-12-06 04:28:11 -0300

    Add return type

--- dilp/dilp.py
+++ dilp/dilp.py
@@ -26,11 +26,11 @@ def get_random() -> dict[str, str]:
     return r.json()
 
 
-def search_word(name: str, number: Optional[int] = 0):
+def search_word(name: str, number: Optional[int] = 0) -> list[str]:
     r = requests.get(f"{BASE_URL}/word/{name}/{number}")
     return r.json()
 
 
-def search_prefix(name: str):
+def search_prefix(name: str) -> list[str]:
     r = requests.get(f"{BASE_URL}/prefix/{name}")
     return r.json()
