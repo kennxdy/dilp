d1c767dc172d52bab355fefb083e5217d5054bdb
Kennedy <kennedyinu@gmail.com>
2023-12-08 10:46:41 -0300

    Fix function name

--- dilp/__init__.py
+++ dilp/__init__.py
@@ -1,10 +1,10 @@
 from .dilp import (
     get_meta,
-    get_near,
     get_news,
     get_random,
     get_word,
     search_infix,
+    search_near,
     search_prefix,
     search_suffix,
     search_word,
@@ -12,11 +12,11 @@ from .dilp import (
 
 __all__ = [
     "get_meta",
-    "get_near",
     "get_news",
     "get_random",
     "get_word",
     "search_infix",
+    "search_near",
     "search_prefix",
     "search_suffix",
     "search_word",
